79a2c530178fffeaa481180237ebfb19b9c5dcaf
yildirimkadir <yildirimkad34@gmail.com>
2022-10-23 23:26:34 +0200

    "Das Projekt wurde erstellt."

---
+++ stock/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import Transaction
+
+
+@receiver(pre_save, sender=Transaction)
+def calculate_total_price(sender, instance, **kwargs):
+    if not instance.price_total:
+        instance.price_total = instance.quantity * instance.price
